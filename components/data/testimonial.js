f91fe8c2f3effdcb33d92eb4f951a89d2ef71d95
HarshVikram-CedCoss <harshvikramsingh@cedcommerce.com>
2024-04-02 19:12:54 +0530

    #1393

--- components/data/testimonial.js
+++ components/data/testimonial.js
@@ -4,7 +4,7 @@ const feedbacks = [
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Fs3.amazonaws.com%2Fuser-assets.codedamn.com%2F62408d38092b190009e9418e%2F2022-05-24%2Fa68014d7187156360cc8e89263ea824d13a176f0&w=96&q=70&output=webp",
     content:
-      "Great course to understand the react design patterns. Every concept is superbly explained by Piyush.",
+      "Great course to understand React design patterns. Every concept is superbly explained by Piyush.",
     studentProfileLink: "",
     courseId: "02",
   },
@@ -22,7 +22,7 @@ const feedbacks = [
     name: "Hasib Faqiri",
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Fcodedamn.com%2Fassets%2Fimages%2Favatar.png&w=96&q=70&output=webp",
-    content: "I thought I could never learn to code but you changed my mind",
+    content: "I thought I could never learn to code, but you changed my mind.",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -31,7 +31,7 @@ const feedbacks = [
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa-%2FAOh14Gg5XTRRMQMiRxqlIgum7MkmYl0PenYAbNMRDvo4XA&w=96&q=70&output=webp",
     content:
-      "IT IS THE best courses which gives you proper learning regarding react js",
+      "It is the best course which gives you proper learning regarding ReactJS.",
     studentProfileLink: "",
     courseId: "02",
   },
@@ -40,7 +40,7 @@ const feedbacks = [
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa-%2FAOh14GilsFerMFQWU_3v5dgTDSQE5wXjSMXqKOfvHdrUlg&w=96&q=70&output=webp",
     content:
-      "Amazing Course. kudos to piyush for sharing this amazing content Perfect for someone who wants to start Java. Hoping to bring more courses on java.",
+      "Amazing course. Kudos to Piyush for sharing this amazing content. Perfect for someone who wants to start Java. Hoping to bring more courses on Java.",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -48,7 +48,7 @@ const feedbacks = [
     name: "Sohit Mandeliya",
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa%2FALm5wu1j0kmSeJCbDNI2Gwjsg6FlKFMgmSUzMdDIEqZz2A&w=80&fit=cover&h=80&q=70&output=webp",
-    content: "good sessions and coding practise",
+    content: "Good sessions and coding practise",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -56,7 +56,7 @@ const feedbacks = [
     name: "Gowtham Eganathan",
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F51506726%3Fv%3D4&w=80&fit=cover&h=80&q=70&output=webp",
-    content: "excellent beginner friendly course",
+    content: "Excellent beginner friendly course",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -65,7 +65,7 @@ const feedbacks = [
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa-%2FAOh14Gi3OfMCQoQ1g7t1OmU1_bl0X2CXiXnAR6tO4xsM66I&w=80&fit=cover&h=80&q=70&output=webp",
     content:
-      "all the basics covered to startwith can say a beginner to pro course🔥",
+      "All the basics covered to startwith can say a beginner to pro course🔥",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -98,7 +98,7 @@ const feedbacks = [
     name: "Dnyanesh Teli",
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa%2FALm5wu2MM0UqqzuRWSIHudFn-t6sCL9Kr3KRHAN62Kr3ww&w=96&q=70&output=webp",
-    content: "helpful for java beginner",
+    content: "Helpful for java beginner",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -106,7 +106,7 @@ const feedbacks = [
     name: "Stuart Markus",
     userImage: null,
     content:
-      "I am enjoying your Ultimate React Design Patterns course, but I find only the older React is done with Javascript in the work place, and would like to see a lot more React taught with Typescript ... ",
+      "I am enjoying your ultimate React design patterns course, but I find only the older React is done with Javascript in the work place, and would like to see a lot more React taught with Typescript. ",
     studentProfileLink: "",
     courseId: "02",
   },
@@ -115,7 +115,7 @@ const feedbacks = [
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Fs3.amazonaws.com%2Fuser-assets.codedamn.com%2F636bd8726d5fca0008043bae%2F2022-11-12%2Fe5342f077b704a6beb4a9242c8d368aad74ddcf9&w=80&q=70&output=webp",
 
-    content: "the examples explained here are really nice! understood well!",
+    content: "The examples explained here are really nice! Understood well!",
     studentProfileLink: "",
     courseId: "01",
   },
@@ -124,7 +124,7 @@ const feedbacks = [
     userImage:
       "https://wsrv.nl/?url=https%3A%2F%2Fs3.amazonaws.com%2Fuser-assets.codedamn.com%2F620d7588246dd0000964cef8%2F2022-10-21%2Fd605514f22da24e22ab8cb39059afa7ae196c3af&w=96&q=70&output=webp",
 
-    content: "course content is really good and easily understandable.",
+    content: "Course Content is really good and easily understandable.",
     studentProfileLink: "",
     courseId: "04",
   },
