218979236cbc51c436c421df184304bd70ad1e55
Sujit Ale Magar <128675355+Sujitale07@users.noreply.github.com>
2024-05-11 17:23:20 +0545

    Text overlap  fixed in homepage

--- components/UI/PortfolioItem.jsx
+++ components/UI/PortfolioItem.jsx
@@ -15,53 +15,57 @@ const PortfolioItem = (props) => {
     ribbonText = null,
   } = props.item;
   return (
-    <div className={`${classes.portfolio__item}`}>
-      <a
-        target='_blank'
-        style={{ textDecoration: 'none' }}
-        href={liveUrl}
-        rel='noreferrer'
-      >
-        <>
-          {ribbonText && (
-            <div style={{ zIndex: 99 }} className='ribbon ribbon-top-left'>
-              <span>{ribbonText}</span>
-            </div>
-          )}
+    <div className={`${classes.portfolio__item}`} style={{ position: 'relative' }}>
+  <a
+    target='_blank'
+    style={{ textDecoration: 'none' }}
+    href={liveUrl}
+    rel='noreferrer'
+  >
+    <>
+      {ribbonText && (
+        <div style={{ zIndex: 99, position: 'absolute', top: 0, left: 0 }} className='ribbon ribbon-top-left'>
+          <span>{ribbonText}</span>
+        </div>
+      )}
 
-          <div className='bg-transparent'>
-            <div className={`${classes.portfolio__img}`}>
-              <Image alt={title} src={img} width={380} height={1} style={{maxHeight: "380px", overflow:"auto"}}/>
+      <div className='bg-transparent'>
+        <div className={`${classes.portfolio__img}`}>
+          <Image alt={title} src={img} width={380} height={1} style={{ maxHeight: "380px", overflow: "auto" }} />
+        </div>
 
-            </div>
+        <div style={{ marginBottom: '10px' }}>
+          <h3 style={{ background: "transparent" }}>{title}</h3>
+          <p style={{ background: "transparent" }}>{subtitle}</p>
+        </div>
+        <div className="pb-5"></div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "20px",                        
+            background: "transparent",
+            display: "flex",
+            flexDirection: "row",            
+            flexWrap: "wrap",
+          }}
+        >
+          {keyword.map((item, index) => (
+            <span
+              className={`${classes.portfolio__keyword} my-1`}
+              key={index}
+              style={{ marginRight: '5px', marginBottom: '5px' }} // Add margin to separate keywords
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  </a>
+</div>
 
-            <h3 style={{ background: "transparent" }}>{title}</h3>
-            <p style={{ background: "transparent", }}>{subtitle}</p>
-            
-            <div className=" w-[100%] mt-5 lg:mt-0"> </div>
-            <div
-              style={{
-                position: "absolute",
-                background: "transparent",
-                bottom: "20px",
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}>
 
-              {keyword.map((item, index) => (
-                <span
-                  className={`${classes.portfolio__keyword} my-1`}
-                  key={index}
-                >
-                  {item}
-                </span>
-              ))}
-            </div>
-          </div>
-        </>
-      </a>
-    </div>
+
   );
 };
 
